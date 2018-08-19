08570340f506fe1fe3987edb6623e92e2cd12c09
rezanur <rezanur@uii.ac.id>
2018-08-19 13:14:32 +0700

    select with join table

--- mainApplication.go
+++ mainApplication.go
@@ -10,18 +10,25 @@ import (
 
 // Book struct
 type Book struct {
-	idBook          string
-	bookName        string
-	bookWriter      string
-	bookPublisher   string
-	bookDescription string
-	idCategoryBook  string
+	idBook           string
+	bookName         string
+	bookWriter       string
+	bookPublisher    string
+	bookDescription  string
+	idCategoryBook   string
+	bookCategoryName string
+}
+
+// CategoryBook struct
+type CategoryBook struct {
+	idCategoryBook   string
+	bookCategoryName string
 }
 
 // dbConnection function does config connection to database
 func dbConnection() (db *sql.DB) {
 	// Configure MySQL Connetion
-	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/bookstore")
+	db, err := sql.Open("mysql", "root:reza@tcp(127.0.0.1:3306)/bookstore")
 
 	if err != nil {
 		panic(err.Error())
@@ -33,13 +40,14 @@ func dbConnection() (db *sql.DB) {
 // Main function
 func main() {
 	fmt.Println("test")
-	getAllBooks() // Method GET All Books From DB
+	getAllBooks()         // Method GET All Books From DB
+	getAllCategoryBooks() // Method GET ALl Category Books From DB
 }
 
 // getAllBooks does function get all data from book table
 func getAllBooks() {
 	db := dbConnection()
-	results, err := db.Query("SELECT * FROM bookstore.book;")
+	results, err := db.Query("SELECT * FROM bookstore.book LEFT JOIN book_category ON book_category.id_book_category = book.id_book_category")
 
 	if err != nil {
 		panic(err.Error())
@@ -48,7 +56,7 @@ func getAllBooks() {
 	for results.Next() {
 		var book Book
 		err = results.Scan(&book.idBook, &book.bookName, &book.bookWriter,
-			&book.bookPublisher, &book.bookDescription, &book.idCategoryBook)
+			&book.bookPublisher, &book.bookDescription, &book.idCategoryBook, &book.idCategoryBook, &book.bookCategoryName)
 
 		if err != nil {
 			panic(err.Error())
@@ -58,5 +66,27 @@ func getAllBooks() {
 		log.Println("Books writer : ", book.bookWriter)
 		log.Println("Books publisher : ", book.bookPublisher)
 		log.Println("Books description : ", book.bookDescription)
+		log.Println("Books Category :", book.bookCategoryName, "\n")
+	}
+}
+
+func getAllCategoryBooks() {
+	db := dbConnection()
+
+	var categoryBook CategoryBook
+	results, err := db.Query("SELECT * FROM bookstore.book_category;")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	for results.Next() {
+		err = results.Scan(&categoryBook.bookCategoryName, &categoryBook.bookCategoryName)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		log.Println("Books Category Name:", categoryBook.bookCategoryName)
 	}
 }
